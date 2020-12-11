98311381def28a26615eed0dd9e3774cbd8983d3
harshdeepy205 <harshdeep205@outlook.com>
2020-12-11 17:55:15 +0530

    'post-issue-solve'

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -10,6 +10,7 @@ router.get('/allposts', requireLogin, (req, res) => {
     Post.find()
         .populate('postedBy', '_id name')
         .populate("comments.postedBy", "_id name")
+        .sort('-createdAt')
         .then(posts => {
             res.json({ posts })
         })
@@ -22,6 +23,7 @@ router.get('/getfolposts', requireLogin, (req, res) => {
     Post.find({ postedBy: { $in: req.user.following } })
         .populate('postedBy', '_id name')
         .populate("comments.postedBy", "_id name")
+        .sort('-createdAt')
         .then(posts => {
             res.json({ posts })
         })
